44a422de719392108d070820ae7a267f3a653a09
Matt Antonelli <contact@raelys.com>
2023-09-19 10:10:53 -0400

    Clean up boilerplate

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tic Tac Toe</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>Tic Tac Toe</h1>
+        <p>We gaming!</p>
+      </main>
+    </>
+  )
+}
